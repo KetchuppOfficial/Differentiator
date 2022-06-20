90eb9beb2e152670fd746b8ac064c24f665ce181
KetchuppOfficial <dolgov04@list.ru>
2022-06-20 17:44:20 +0300

    Useless commented part was removed

--- include/Differentiator.h
+++ include/Differentiator.h
@@ -95,26 +95,10 @@ static const struct Function Functions_Data_Base[] =
     {-1, ""}
 };
 
-struct Token *Lexer (const char *file_name, int *n_tokens);
-struct Node *Parser (const struct Token *token_arr, const int n_tokens);
-int Tree_Destructor (struct Node *node_ptr);
-int Differentiator (struct Node *root);
-int Tree_Dump   (struct Node *root_ptr, const char *text_file_name, const char *image_file_name, const char *var);
+struct Token *Lexer           (const char *file_name, int *n_tokens);
+struct Node  *Parser          (const struct Token *token_arr, const int n_tokens);
+int           Tree_Destructor (struct Node *node_ptr);
+int           Differentiator  (const struct Node *root);
+int           Tree_Dump       (const struct Node *root_ptr, const char *text_file_name, const char *image_file_name, const char *var);
 
-//*******************************************************************************************************************
-//FUNCTION PROTOTYPES
-#if 0
-
-int    Check_Fractions    (struct Node *node_ptr);
-int    Simplify           (struct Node **node_ptr, const char *var);
-int    Simplify_Function  (struct Node **root_ptr, const char *var);
-int    Compare_Trees      (const struct Node *tree_1, const struct Node *tree_2);
-double Calc_Expression    (const struct Node *node_ptr);
-
-int Dump_In_Tex  (const struct Node *orig_tree, struct Node **tree_arr, char **vars_arr,
-                  const int n_vars, const char *text_file_name);
-int Formula_Dump (const struct Node *node_ptr, FILE *tex_file);
 #endif
-//*******************************************************************************************************************
-
-#endif // DIFFERENTIATOR_H_INCLUDED
